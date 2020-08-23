6ade4da2802af78da1163a8d43435cc0dc83c939
Joe Winterburn <jjw79@cam.ac.uk>
2020-08-23 18:28:38 +0100

    day 10

--- pages/D/day10.js
+++ pages/D/day10.js
@@ -16,6 +16,42 @@ const Day10 = () => {
           event={"Breakfast"}
           location={"RAU Dining Hall"}
         />
+
+        <TimetableBlock
+          time={"9:30 - 11:15"}
+          event={"Mock Admissions Test"}
+          location={"Locke Room"}
+        />
+
+        <TimetableBlock
+          time={"11:30 - 12:15"}
+          event={"Panel with Martin Cox, Robin Koerner and Jamie Whyte"}
+          location={"Caplan Lecture Theatre"}
+        />
+
+        <TimetableBlock
+          time={"12:30 - 13:30"}
+          event={"LUNCH"}
+          location={"RAU Dining Hall"}
+        />
+
+        <TimetableBlock
+          time={"13:30 - 14:45"}
+          event={"Panel with Martin Cox, Robin Koerner and Jamie Whyte"}
+          location={"Caplan Lecture Theatre"}
+        />
+
+        <TimetableBlock
+          time={"17:00 - 18:30"}
+          event={"Concert & Awards Ceremony"}
+          location={"Chapel"}
+        />
+
+        <TimetableBlock
+          time={"18:45 - 20:15"}
+          event={"Farewell Dinner"}
+          location={"RAU Dining Hall"}
+        />
       </div>
     </>
   )
